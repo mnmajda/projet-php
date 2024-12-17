a35be30286ef9b2f83600414cbc5444c98f7d5b5
Majda Mnasri <majdamnasri@gmail.com>
2024-12-17 20:26:20 +0100

    update title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once 'functions.php'; // Inclut les fonctions
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Age Calculator</title>
+    <title>Age Calculator !!</title>
     <!-- Lien vers le fichier CSS externe -->
     <link rel="stylesheet" href="style.css">
 </head>
